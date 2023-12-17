b687208b65708e1773a2afddeb77f284f5958f10
Gaurav Agam <gaurav.agam11@gmail.com>
2023-12-17 19:48:53 +0530

    displayed nfts list

--- src/App.jsx
+++ src/App.jsx
@@ -26,9 +26,11 @@ function App() {
   const { address, isConnected } = useAccount();
   const { chain } = useNetwork();
   const [error, setError] = useState('');
-  const toast= useToast()
+  const toast= useToast();
+  const [nftData,setNftData] = useState([]);
+  const [nftMetadataObjects,setNftMetadataObjects] = useState([]);
   
-  async function getTokenBalance(userAddress) {
+  async function getTokenBalance() {
     try{
       setError('');
       setIsLoading(true)
@@ -38,7 +40,7 @@ function App() {
       };
 
       const alchemy = new Alchemy(config);
-      const data = await alchemy.core.getTokenBalances(userAddress);
+      const data = await alchemy.core.getTokenBalances(address);
   
       setResults(data);
   
@@ -69,7 +71,7 @@ function App() {
 
   useEffect(()=>{
     if(isConnected){
-      getTokenBalance(address);
+      getTokenBalance();
     }
   },[isConnected,address,chain])
 
@@ -85,11 +87,45 @@ function App() {
         return Network.ETH_MAINNET
     }
   }
+
+  const getNfts = async()=>{
+    try {
+      setError('');
+      setIsLoading(true)
+      const config = {
+        apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
+        network: getNetworkForConfigByChain(chain.name),
+      };
+
+      const alchemy = new Alchemy(config);
+      const nftData = await alchemy.nft.getNftsForOwner(address);
+      setNftData(nftData);
+      const nftMetadataPromises = [];
+      for(let i=0;i<nftData.ownedNfts.length;i++){
+        const nftMetadata = alchemy.nft.getNftMetadata(nftData.ownedNfts[i].contract.address,nftData.ownedNfts[i].tokenId,nftData.ownedNfts[i].tokenType);
+        nftMetadataPromises.push(nftMetadata);
+      }
+      setNftMetadataObjects(await Promise.all(nftMetadataPromises));
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+      toast({
+        title: "Error",
+        description: error.message,
+        status: "error",
+        isClosable: true,
+        duration: 9000,
+        position: 'top-right'
+      })
+    }
+  }
+
   return (
     <Box w="100%">
       <Box w="100%" p={4} borderWidth='1px' bg={"#E2E8F0"} height={"fit-content"}>
 	  	  <Heading fontSize={24} color="#8AAAE5" as="h1">
-            ERC-20 Token Indexer
+            ERC-20 Token & NFT Indexer
         </Heading>
       </Box>
       <Center mt={"20px"}>
@@ -99,7 +135,7 @@ function App() {
           flexDirection={'column'}
         >
         <Text paddingX={[4,0]}>
-          Connect your wallet and this website will return all of its ERC-20 token balances!
+          Connect your wallet and this website will return all of its ERC-20 token balances & NFTs!
         </Text>
         <ConnectButton accountStatus={{
           smallScreen: 'avatar',
@@ -110,20 +146,20 @@ function App() {
         } />
         </Flex>
         </Center>
-        <Tabs variant="soft-rounded" colorScheme={"blue"} paddingX={[4,"50px"]}>
+        { isConnected && <Tabs variant="enclosed" isFitted paddingX={[4,"50px"]} paddingY="40px" isLazy>
           <TabList>
             <Tab color={"#000000"}>ERC-20 Token</Tab>
-            <Tab>NFT</Tab>
+            <Tab onClick={getNfts}>NFT</Tab>
           </TabList>
           <TabPanels>
             <TabPanel paddingX={"0px"}>
-              { isConnected && <Flex
+               <Flex
                 w="100%"
                 flexDirection="column"
                 alignItems="center"
                 justifyContent={'center'}
               >
-                { !error && <><Heading my={8} fontSize={24}>ERC-20 token balances:</Heading>
+                { !error && <><Heading mb={8} fontSize={24}>ERC-20 token balances:</Heading>
                 {!isLoading ? (
                   <Flex columnGap={"12px"} rowGap={"15px"} wrap="wrap">
                     {results.tokenBalances.map((e, i) => {
@@ -154,10 +190,48 @@ function App() {
                   </Flex>
                 ) : 
                 <Spinner size="xl" thickness='4px'/>}</>}
-              </Flex>}
+              </Flex>
+            </TabPanel>
+            <TabPanel>
+            <Flex
+                w="100%"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent={'center'}
+              >
+                { !error && <><Heading mb={8} fontSize={24}>NFTs:</Heading>
+                {!isLoading ? (
+                  <Flex columnGap={"12px"} rowGap={"15px"} wrap="wrap">
+                    {nftData?.ownedNfts?.length ? nftData.ownedNfts.map((e, i) => {
+                      return (
+                        <Flex
+                          flexDir={'column'}
+                          color="#000000"
+                          bg="#E2E8F0"
+                          key={e.contractAddress}
+                          p="15px"
+                          width={["100%","210px"]}
+                        >
+                          <Box>
+                            <b>Name:</b> {nftMetadataObjects[i]?.name?.length ===0 ? 'No Name' : nftMetadataObjects[i]?.name}&nbsp;
+                          </Box>
+                          {["image/png","image/jpeg","image/jpg","image/svg+xml","image/svg"].includes(nftMetadataObjects[i].image.contentType) ? 
+                            <Image src={nftMetadataObjects[i]?.image?.originalUrl ?? 'https://via.placeholder.com/200'} />
+                            : <video>
+                              <source src={nftMetadataObjects[i]?.image?.originalUrl} type={nftMetadataObjects[i].image.contentType}></source>
+                            </video>
+                          }
+                        </Flex>
+                      );
+                    }) : ' No NFTs'}
+                  
+                  </Flex>
+                ) : 
+                <Spinner size="xl" thickness='4px'/>}</>}
+              </Flex>
             </TabPanel>
           </TabPanels>
-        </Tabs>
+        </Tabs>}
       
     </Box>
   );
